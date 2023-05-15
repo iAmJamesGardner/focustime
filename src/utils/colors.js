7ca8a470da41f991f0c4df343e9a956daefcb990
James <developer@cryptobaseatm.com>
2023-05-15 18:29:30 -0400

    Initial Entry

---
+++ src/utils/colors.js
@@ -0,0 +1,12 @@
+export const colors = {
+  white: '#fff',
+  darkBlue: '#252250',
+  red: '#ff0000',
+  black: '#000000',
+  blue: '#0000FF',
+  gray: '#808080',
+  green: '#008000',
+  purple: '#800080',
+  yellow: '#FFFF00',
+  progressBar: '#5e84e2',
+};
